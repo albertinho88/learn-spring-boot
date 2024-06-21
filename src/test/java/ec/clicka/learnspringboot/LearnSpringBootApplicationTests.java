47e57d8f619981f7428a302a7da0467eb22f18a0
Jlarragamena <jlarragamena@trueblue.com>
2024-06-21 16:48:56 -0500

    Initial commit

---
+++ src/test/java/ec/clicka/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.clicka.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
